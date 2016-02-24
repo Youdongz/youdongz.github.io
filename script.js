9b1d8029e70c07a0efc895b5d74f8eaf784b541b
Youdong Zhang <youdongzhang@Youdongs-MacBook-Air.local>
2016-02-24 01:22:11 -0800

    rawr

---
+++ script.js
@@ -0,0 +1,23 @@
+function passWord() {
+	var testV = 1;
+	var pass1 = prompt('Please Enter Your Password',' ');
+	while (testV < 3) {
+		if (!pass1) 
+			history.go(-1);
+		if (pass1.toLowerCase() == "letmein") {
+			display();
+			break;
+		} 
+		testV+=1;
+		var pass1 = 
+		prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+	}
+	if (pass1.toLowerCase()!="password" & testV ==3) 
+		history.go(-1);
+	return " ";
+} 
+
+function display() {
+	document.getElementById('origin').style.cssText = 'display: initial';
+	document.getElementById('lock').style.cssText = 'display: none';
+}
