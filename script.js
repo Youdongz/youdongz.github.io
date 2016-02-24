6b9b0090d2aa0bee741c7e0793119d8ea9981de4
Youdong Zhang <youdongzhang@airbears2-10-142-100-192.airbears2.1918.berkeley.edu>
2016-02-24 15:45:57 -0800

    password problem

--- script.js
+++ script.js
@@ -6,19 +6,39 @@ function passWord() {
 			history.go(-1);
 		else if (pass1.toLowerCase() == "letmein") {
 			display();
+		break;
+		} else if (pass1.toLowerCase() == "admin") {
+			admin();
 			break;
 		} else {
 			testV+=1;
 			var pass1 = ""
-			prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+			prompt('Access Denied - Password Incorrect, Please Try Again.', '');
 		}
 	}
 	if (pass1.toLowerCase()!="password" & testV ==3) 
 		history.go(-1);
 	return " ";
-} 
+}
 
 function display() {
 	document.getElementById('origin').style.cssText = 'display: initial';
 	document.getElementById('lock').style.cssText = 'display: none';
+}
+
+function admin() {
+	document.getElementById('origin').style.cssText = 'display: initial';
+	document.getElementById('lock').style.cssText = 'display: none';
+	document.getElementById('onoffswitch').style.cssText = 'display: initial';
+}
+
+function switch_it() {
+	var x = document.getElementById("myonoffswitch").checked;
+	if (x == false) {
+		document.getElementById('green').style.cssText = 'display: none';
+		document.getElementById('red').style.cssText = 'display: initial';
+	} else {
+		document.getElementById('green').style.cssText = 'display: initial';
+		document.getElementById('red').style.cssText = 'display: none';
+	}
 }
