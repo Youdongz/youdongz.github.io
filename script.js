8ed022a22bd1d91a146657ee74892c5e1ec76004
Youdong Zhang <youdongzhang@Youdongs-MacBook-Air.local>
2016-02-24 13:37:07 -0800

    password problem

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function passWord() {
 	while (testV < 3) {
 		if (!pass1) 
 			history.go(-1);
-		if (pass1.toLowerCase() == "letmein") {
+		else if (pass1.toLowerCase() == "letmein") {
 			display();
 			break;
-		} 
-		testV+=1;
-		var pass1 = 
-		prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+		} else {
+			testV+=1;
+			var pass1 = 
+			prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+		}
 	}
 	if (pass1.toLowerCase()!="password" & testV ==3) 
 		history.go(-1);
